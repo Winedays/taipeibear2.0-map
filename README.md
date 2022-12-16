5d5a73d4bfde3ff48560128f2560a7221bfe80bc
Winedays <adler011014@gmail.com>
2022-12-17 00:51:26 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# 熊好券 2.0 Google 地圖
+從熊好券官網爬蟲拿到每種券的適用店家資料(自動)，再匯入到 google map 做出標記(手動)  
+**為了省爬蟲的時間所以只有拿店家的名字給 google map 做標記，所以多少會出現一些地點錯誤的標記**  
+有一些店家可能因為登記的名字跟 google map 上的名字不完全一樣，所以可能會因為找不到而沒出現在地圖上  
+因為 google map 對每個圖層及整張地圖的標記數量有限制，所以饗購券地圖只好分成兩張，而且部分行政區會用兩個圖層來列出所有店家  
+**地圖資訊僅供參考用**  
+
+* 市場通用券: https://www.google.com/maps/d/u/0/edit?mid=1m3WOd8_aZjvzQH7iQWMYymjgvSJndsU&usp=sharing
+<!-- https://reurl.cc/4XqZvX -->
+* 饗購券(北投/內湖/南港區**以外**): https://www.google.com/maps/d/u/0/edit?mid=1CWQXFGkSWiwvhRo47GtsfF-BZCNnZ1A&usp=sharing
+<!-- https://reurl.cc/OEz9Lg -->
+* 饗購券(北投/內湖/南港區): https://www.google.com/maps/d/u/0/edit?mid=1QHOhfT4oEBHhQeBFgn0yI1__woA8pUI&usp=sharing
+<!-- https://reurl.cc/eWn177 -->
---
+++ search.py
@@ -0,0 +1,137 @@
+import os
+import time
+from typing import List
+from enum import Enum
+import csv
+
+import requests
+
+BASE_URL = "https://tpservice.gov.taipei"
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+
+class COUPON_TYPE(Enum):
+    STAY = 61
+    BUY = 63
+    SPORT = 64
+    ART = 62
+    PLAY = 66  # 農好券
+    EAT = 52  # 市集券
+
+class DISTRICT(Enum):
+    zhongzheng = '中正區'
+    datong = '大同區'
+    zhongshan = '中山區'
+    songshan = '松山區'
+    daan = '大安區'
+    wanhua = '萬華區'
+    xinyi = '信義區'
+    shilin = '信義區'
+    beitou = '北投區'
+    neihu = '內湖區'
+    nangang = '南港區'
+    wenshan = '文山區'
+
+def newSession():
+    session = requests.Session()
+    session.headers['User-Agent'] = USER_AGENT
+    session.headers['origin'] = 'https://tpservice.gov.taipei'
+    session.headers['referer'] = 'https://tpservice.gov.taipei/'
+    session.headers['authorization'] = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0YWlwZWkiLCJqdGkiOiJmY2I3M2EyZS1lYTZiLTQ2MDEtYTM0OC05NjJmNTZjOTY5ZmMiLCJyb2xlcyI6WyJBZG1pbiIsIlVzZXJzIl0sIm5iZiI6MTYzMzY2MDc4MywiZXhwIjoxOTQ5MTkzNTgzLCJpYXQiOjE2MzM2NjA3ODMsImlzcyI6Ikp3dFRhaXBlaSJ9.cTyWFhmd3moXltT3nv4xroxFE0fMcZzKqubrI4R7Dps'
+    resp = session.get(BASE_URL)
+    if resp.status_code != 200:
+        raise ConnectionError(f'status code: {resp.status_code}')
+
+    return session
+
+session = newSession()
+
+def search_coupon(coupon: COUPON_TYPE, district: DISTRICT):
+    lng = 24.0334334
+    lat = 120.5006263
+    result = []
+
+    url = f'{BASE_URL}:5003/api/WebStore/search'
+    page = 1
+    while True:
+        print(f'search page {page}...')
+        params = {
+            'Lng': lng,
+            'Lat': lat,
+            'CouponTypes': coupon.value,
+            'Districts': district.value,
+            'Page': page,
+            'PerPageLimt': 120,
+        }
+        resp = session.get(url, params=params)
+        if resp.status_code != 200:
+            raise ConnectionError(f'status code: {resp.status_code}')
+    
+        for s in resp.json().get('list', []):
+            id = s.get('id', None)
+            name = s.get('name', None)
+            if id and name:
+                result.append({
+                    'id': id, 
+                    'name': name,
+                })
+
+        if resp.json().get('totalPages', page) <= page:
+            break
+
+        page += 1
+        time.sleep(0.1)
+
+    return result
+    '''
+    sample response : 
+    {
+    'totalPages': 1,
+    'currentPage': 1,
+    'itemsPerPage': 1,
+    'totals': 1,
+    'oriTotals': 0,
+    'list': [
+        {
+        'id': '00022dda-e7d5-43a3-b059-b2d3892efd9f',
+        'name': '蘭州38號攤',
+        'bannerImageURL': None,
+        'earlyBird': None,
+        'voucherTypeList': [
+            49,
+            52
+        ]
+        }
+    ]
+    }
+    '''
+
+
+def save(type: COUPON_TYPE, district: DISTRICT, shops: List):
+    print('save csv...')
+    title = ['ID', '行政區', '地址', '店名']
+    if not os.path.exists(type.name):
+        os.makedirs(type.name)
+    with open(f'{type.name}/{district.value}.csv', 'w', encoding='utf-8', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(title)
+        for shop in shops:
+            writer.writerow([
+                shop['id'], 
+                district.value, 
+                f'台北市{district.value}{shop["name"]}',
+                shop['name']
+            ])
+
+
+def main():
+    for coupon in COUPON_TYPE:
+        print(f'start {coupon.name}...')
+        for district in DISTRICT:
+            print(f'start {district.value}...')
+            shops = search_coupon(coupon, district)
+            save(coupon, district, shops)
+    print('success!')
+
+
+if __name__ == '__main__':
+    main()
